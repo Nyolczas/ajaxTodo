25aa5897c40c4b0ab475e83aaeea4891d96c25c8
Nyolczas <nyolczas@gmail.com>
2019-04-29 20:47:51 +0200

    feltöltésig
    egyelőre csak a beolvasás működik.

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,50 @@
 $(function () {
-  
+
+    var $feladatok = $('#feladatok');
+    var $feladat = $('#feladat');
+    var $importance = $('#importance');
+    var $urgency = $('#urgency');
+
+    function feladatHozzaadas(feladat) {
+        $feladatok.append('<li>Feladat: ' + feladat.feladat +
+                    ', jelentőség: ' + feladat.importance +
+                    ', státusz: ' + feladat.urgency + '</li>');
+    }
+
     $.ajax({
         type: 'GET',
-        url: '/api//backend.json',
-        success: function(data) {
-            console.log('success', data);
+        url: 'backend.json',
+        success: function (feladatok) {
+            console.log('success', feladatok);
+            $.each(feladatok, function (i, teendo) {
+                feladatHozzaadas(teendo);
+            });
+        },
+        error: function() {
+            alert('Hiba történt az adatok betöltésénél!');
         }
     });
 
-})
+    $('#add').on('click', function() {
+
+        var teendo = {
+            feladat: $feladat.val(),
+            importance: $importance.val(),
+            urgency: $urgency.val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'backend.json',
+            data: teendo,
+            success: function(ujTeendo) {
+                feladatHozzaadas(ujTeendo);
+            },
+            error: function() {
+                alert('Hiba történt az adatok mentésénél!');
+            }
+        });
+        
+    });
+
+});
